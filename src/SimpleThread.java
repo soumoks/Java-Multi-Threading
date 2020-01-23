b0a85a15f244b66fe70069c6adf72d9400568759
soumoks <soumoks@gmail.com>
2020-01-23 11:42:07 -0700

    Storing random number in Collection instead of object

--- src/SimpleThread.java
+++ src/SimpleThread.java
@@ -1,14 +1,32 @@
+import java.util.ArrayList;
 import java.util.Random;
 
 public class SimpleThread implements Runnable {
-    private int randomNum;
+    //private int randomNum;
+    private ArrayList<Integer> randomNumList;
 
     /**
      * Getter for the randomNum member field
      * @return
      */
-    public int getRandomNum(){
-        return randomNum;
+//    public int getRandomNum(){
+//        return randomNum;
+//    }
+
+    /**
+     * Getter for randomNum Array List
+     * @return
+     */
+    public ArrayList<Integer> getRandomNumList(){
+        return randomNumList;
+    }
+
+    /**
+     * Default constructor.
+     * This constructor initialises the ArrayList Object
+     */
+    public SimpleThread(){
+        randomNumList = new ArrayList<>();
     }
 
     /**
@@ -17,7 +35,9 @@ public class SimpleThread implements Runnable {
     @Override
     public void run() {
         Random rand = new Random();
-        randomNum = rand.nextInt(101);
-        System.out.println("Random Number: " +  randomNum);
+        //randomNum = rand.nextInt(101);
+        randomNumList.add(rand.nextInt(101));
+        //Prints the last element in the ArrayList
+        System.out.println("Random Number: " +  randomNumList.get(randomNumList.size() -1));
     }
 }
--- src/ThreadApp.java
+++ src/ThreadApp.java
@@ -1,13 +1,19 @@
-public class ThreadApp {
+import java.util.ArrayList;
 
+public class ThreadApp {
     public static void main(String [] args) throws InterruptedException {
         int sum = 0;
+        SimpleThread obj = new SimpleThread();
         for(int i=0;i<5;i++){
-            SimpleThread obj = new SimpleThread();
             Thread t = new Thread(obj);
             t.start();
             t.join();
-            sum+=obj.getRandomNum();
+        }
+        /*
+        Generate the sum present in the SimpleThread ArrayList.
+         */
+        for(Integer temp: obj.getRandomNumList()){
+            sum+=temp;
         }
         System.out.println("Sum of random numbers: " + sum);
     }
